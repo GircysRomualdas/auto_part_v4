65f40c9de5b10f6a26e3d3f9e90b28fffed97bbc
Romas <r2udis@gmail.com>
2024-12-17 09:46:28 +0000

    add admin user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    if admin_signed_in?
+      redirect_to admin_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :customers
 
   get "home/index"
+  get "admin", to: "admin#index"
 
   root "home#index"
 end
